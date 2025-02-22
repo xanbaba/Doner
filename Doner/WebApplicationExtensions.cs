e70bcdc1daeede60de5a45bcd90d87531b6e34e6
XanbabaF <xanbabaf@gmail.com>
2025-02-22 16:14:16 +0400

    Adding feature support

---
+++ Doner/WebApplicationExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Doner.Features;
+
+namespace Doner;
+
+public static class WebApplicationExtensions
+{
+    public static void AddFeature<TFeature>(this WebApplicationBuilder builder) where TFeature : IFeature
+    {
+        TFeature.Build(builder);
+    }
+
+    public static void UseFeature<TFeature>(this WebApplication app) where TFeature : IFeature
+    {
+        TFeature.Configure(app);
+    }
+}

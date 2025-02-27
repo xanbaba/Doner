2b7c952c9f372e3fd02fc564f7a4f90e473165a9
Bombastic <Tima.salmanov@icloud.com>
2025-02-27 19:58:09 +0400

    registered db context

--- Doner/Program.cs
+++ Doner/Program.cs
@@ -1,10 +1,16 @@
+using Doner.DataBase;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddDbContextFactory<AppDbContext>();
+
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())

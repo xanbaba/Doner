b3a6332354e13e2702b4ac45855239073ab839aa
XanbabaF <xanbabaf@gmail.com>
2025-02-27 21:23:25 +0400

    defining endpoint mapper and endpoint routes

---
+++ Doner/Features/AuthFeature/AuthEndpointMapper.cs
@@ -0,0 +1,11 @@
+﻿namespace Doner.Features.AuthFeature;
+
+public class AuthEndpointMapper : IEndpointMapper
+{
+    public static void Map(IEndpointRouteBuilder builder)
+    {
+        builder.MapGet("/sign-in", () => { });
+        builder.MapGet("/sign-up", () => { });
+        builder.MapGet("/refresh", () => { });
+    }
+}

0af5b2cb1ea37b6d6cd16d279c945cc855556f1b
Bombastic <Tima.salmanov@icloud.com>
2025-02-27 21:18:50 +0400

    created workspace feature

---
+++ Doner/Features/WorkspaceFeature/WorkspaceFeature.cs
@@ -0,0 +1,14 @@
+namespace Doner.Features.WorkspaceFeature;
+
+public class WorkspaceFeature: IFeature
+{
+    public static void Build(WebApplicationBuilder builder)
+    {
+        
+    }
+
+    public static void Configure(WebApplication app)
+    {
+        
+    }
+}

0163a52b182ab32d0ea1033259d5d8c69aacf890
XanbabaF <xanbabaf@gmail.com>
2025-02-27 20:47:02 +0400

    Adding Users DbSet

--- Doner/DataBase/AppDbContext.cs
+++ Doner/DataBase/AppDbContext.cs
@@ -1,9 +1,12 @@
+using Doner.Features.AuthFeature;
 using Microsoft.EntityFrameworkCore;
 
 namespace Doner.DataBase;
 
 public class AppDbContext(IConfiguration configuration): DbContext
 {
+    public DbSet<User> Users { get; set; }
+    
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlite(configuration.GetConnectionString("SQLite"));

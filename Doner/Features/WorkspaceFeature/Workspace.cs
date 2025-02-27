a268176e2947b2e00565ad91a3af5208ea786ed4
Bombastic <Tima.salmanov@icloud.com>
2025-02-27 21:29:32 +0400

    created workspace entity

---
+++ Doner/Features/WorkspaceFeature/Workspace.cs
@@ -0,0 +1,18 @@
+using Doner.Features.AuthFeature;
+
+namespace Doner.Features.WorkspaceFeature;
+
+public class Workspace
+{
+    public Guid Id { get; set; }
+    
+    public string Name { get; set; } = null!;
+    public string? Description { get; set; }
+    
+    public Guid OwnerId { get; set; }
+    public User Owner { get; set; } = null!;
+
+    public List<User> Invitees { get; set; } = null!;
+    
+    
+}

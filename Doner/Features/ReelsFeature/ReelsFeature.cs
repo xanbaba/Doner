47df47e83dbe4f60922ba6ce4b4f2189eff95083
Bombastic <Tima.salmanov@icloud.com>
2025-02-28 21:39:06 +0400

    created reels feature

---
+++ Doner/Features/ReelsFeature/ReelsFeature.cs
@@ -0,0 +1,14 @@
+namespace Doner.Features.ReelsFeature;
+
+public class ReelsFeature: IFeature
+{
+    public static void Build(WebApplicationBuilder builder)
+    {
+        
+    }
+
+    public static void Configure(WebApplication app)
+    {
+        
+    }
+}

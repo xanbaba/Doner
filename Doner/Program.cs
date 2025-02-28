6d9ed74ec1db179fbdca59c074e9447f4c0435ad
XanbabaF <xanbabaf@gmail.com>
2025-02-28 19:55:03 +0400

    post merge fix: adding AuthFeature

--- Doner/Program.cs
+++ Doner/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+builder.AddFeature<AuthFeature>();
 builder.Services.AddDbContextFactory<AppDbContext>();
 
 var app = builder.Build();

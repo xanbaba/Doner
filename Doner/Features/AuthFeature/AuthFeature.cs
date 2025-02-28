aefeed923f80f4512b8c3271fbb3ed7adc8d4660
XanbabaF <xanbabaf@gmail.com>
2025-02-28 15:23:53 +0400

    configuring authentication and authorization

--- Doner/Features/AuthFeature/AuthFeature.cs
+++ Doner/Features/AuthFeature/AuthFeature.cs
@@ -1,14 +1,36 @@
-﻿namespace Doner.Features.AuthFeature;
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Doner.Features.AuthFeature;
 
 public class AuthFeature : IFeature
 {
     public static void Build(WebApplicationBuilder builder)
     {
-        
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidIssuer = builder.Configuration.GetJwtIssuer(),
+
+                ValidateAudience = true,
+                ValidAudience = builder.Configuration.GetJwtAudience(),
+
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey =
+                    new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetJwtSecret())),
+
+                ValidateLifetime = true
+            };
+        });
     }
 
     public static void Configure(WebApplication app)
     {
+        app.UseAuthentication();
+        app.UseAuthorization();
         
     }
 }

d82b7fc18021dab352160f676ccea6ac5c34d617
XanbabaF <xanbabaf@gmail.com>
2025-03-01 23:37:44 +0400

    changing refresh token from byte[] implementation to string one for easier user data processing

--- Doner/Features/AuthFeature/RefreshToken.cs
+++ Doner/Features/AuthFeature/RefreshToken.cs
@@ -5,6 +5,6 @@ public class RefreshToken
     public Guid Id { get; set; }
     public Guid UserId { get; set; }
     public User User { get; set; } = null!;
-    public byte[] Token { get; set; } = null!;
+    public string Token { get; set; } = null!;
     public DateTime UtcExpiresAt { get; set; }
 }

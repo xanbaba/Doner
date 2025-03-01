d444e9a4dfcc9486994ab342c48f5352743b304a
XanbabaF <xanbabaf@gmail.com>
2025-03-01 23:35:09 +0400

    Adding refresh token generator based on RandomNumberGenerator

---
+++ Doner/Features/AuthFeature/RefreshTokenGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Cryptography;
+
+namespace Doner.Features.AuthFeature;
+
+public static class RefreshTokenGenerator
+{
+    public static string GenerateRefreshToken()
+    {
+        var refreshToken = RandomNumberGenerator.GetBytes(32);
+        return Convert.ToBase64String(refreshToken);
+    }
+}

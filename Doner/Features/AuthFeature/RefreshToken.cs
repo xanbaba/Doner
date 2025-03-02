8d72526bf18d72bc8f80bdb78e36d6cde162adee
XanbabaF <xanbabaf@gmail.com>
2025-03-02 20:26:05 +0400

    making Id of RefreshToken of int type

--- Doner/Features/AuthFeature/RefreshToken.cs
+++ Doner/Features/AuthFeature/RefreshToken.cs
@@ -2,7 +2,7 @@
 
 public class RefreshToken
 {
-    public Guid Id { get; set; }
+    public int Id { get; set; }
     public Guid UserId { get; set; }
     public User User { get; set; } = null!;
     public string Token { get; set; } = null!;

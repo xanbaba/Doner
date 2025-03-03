7c17aeae49930909047f63ef7d67060defe1c464
XanbabaF <xanbabaf@gmail.com>
2025-03-03 21:24:38 +0400

    Defining Checkbox element

---
+++ Doner/Features/ReelsFeature/Elements/Checkbox.cs
@@ -0,0 +1,7 @@
+﻿namespace Doner.Features.ReelsFeature.Elements;
+
+public class Checkbox() : Element(nameof(Checkbox))
+{
+    public bool IsChecked { get; set; }
+    public string Text { get; set; } = string.Empty;
+}

da67707ea1e071cb5b3d414ad5ee7679337d8dde
Bombastic <Tima.salmanov@icloud.com>
2025-03-14 21:34:46 +0400

    Completed Workspace feature

--- Doner/Features/WorkspaceFeature/WorkspaceFeature.cs
+++ Doner/Features/WorkspaceFeature/WorkspaceFeature.cs
@@ -1,14 +1,16 @@
+using Doner.Features.WorkspaceFeature.Repository;
+
 namespace Doner.Features.WorkspaceFeature;
 
 public class WorkspaceFeature: IFeature
 {
     public static void Build(WebApplicationBuilder builder)
     {
-        
+        builder.Services.AddScoped<IWorkspaceRepository, WorkspaceRepositoryLocal>();
     }
 
     public static void Configure(WebApplication app)
     {
-        
+        app.MapEndpoints<WorkspaceEndpointMapper>();
     }
 }

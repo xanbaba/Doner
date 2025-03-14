cb9b38975f59fe4473b5bea4ee7d848688c6f283
XanbabaF <xanbabaf@gmail.com>
2025-03-14 16:22:21 +0400

    Defining interface for Reel repo

---
+++ Doner/Features/ReelsFeature/Services/IReelRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Doner.Features.ReelsFeature.Services;
+
+public interface IReelRepository
+{
+    Task AddAsync(Reel reel, CancellationToken cancellationToken = default);
+    Task<Reel?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<bool> UpdateAsync(Reel reel, CancellationToken cancellationToken = default);
+    Task<bool> DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<IEnumerable<Reel>> GetAsync(CancellationToken cancellationToken = default);
+    Task<IEnumerable<Reel>> GetByWorkspaceAsync(Guid workspaceId, CancellationToken cancellationToken = default);
+    Task<IEnumerable<Reel>> SearchByNameAsync(string name, SearchOption searchOption, CancellationToken cancellationToken = default);
+}
---
+++ Doner/Features/ReelsFeature/Services/SearchOption.cs
@@ -0,0 +1,16 @@
+﻿namespace Doner.Features.ReelsFeature.Services;
+
+public enum SearchOption
+{
+    None,
+    
+    /// <summary>
+    /// indicates that search query should fully match.
+    /// </summary>
+    FullMatch,
+    
+    /// <summary>
+    /// indicates that search query should be partially matched. Matches should at least contain the query.
+    /// </summary>
+    PartialMatch,
+}

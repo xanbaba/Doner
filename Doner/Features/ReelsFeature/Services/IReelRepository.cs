6feb9b4804fe799d731e6ee1f34bddf34d3a4f47
XanbabaF <xanbabaf@gmail.com>
2025-03-14 16:22:21 +0400

    Defining interface for Reel repo

---
+++ Doner/Features/ReelsFeature/Services/IReelRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Doner.Features.ReelsFeature.Services;
+
+public interface IReelRepository
+{
+    Task<bool> AddAsync(Reel reel);
+    Task<Reel?> GetByIdAsync(Guid id);
+    Task<bool> UpdateAsync(Reel reel);
+    Task<bool> DeleteAsync(Guid id);
+    Task<IEnumerable<Reel>> GetAsync();
+    Task<IEnumerable<Reel>> GetByWorkspaceAsync(Guid workspaceId);
+    Task<IEnumerable<Reel>> SearchByNameAsync(string name, SearchOption searchOption);
+}

43d1be71f658926f89aaa6a4ef35d08e8cc488a7
Bombastic <Tima.salmanov@icloud.com>
2025-03-14 21:31:34 +0400

    Using changes

--- Doner/Features/WorkspaceFeature/Entities/WorkspaceInviteConfiguration.cs
+++ Doner/Features/WorkspaceFeature/Entities/WorkspaceInviteConfiguration.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace Doner.Features.WorkspaceFeature;
+namespace Doner.Features.WorkspaceFeature.Entities;
 
 public class WorkspaceInviteConfiguration : IEntityTypeConfiguration<WorkspaceInvite>
 {

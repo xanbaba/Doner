c4b5b1edb93f4ec2a2669953a6d1e72dd12baf65
XanbabaF <xanbabaf@gmail.com>
2025-03-16 15:15:17 +0400

    Removing redundant method from repo

--- Doner/Features/ReelsFeature/Services/IReelRepository.cs
+++ Doner/Features/ReelsFeature/Services/IReelRepository.cs
@@ -6,7 +6,6 @@ public interface IReelRepository
     Task<Reel?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
     Task<bool> UpdateAsync(Reel reel, CancellationToken cancellationToken = default);
     Task<bool> DeleteAsync(Guid id, CancellationToken cancellationToken = default);
-    Task<IEnumerable<Reel>> GetAsync(CancellationToken cancellationToken = default);
     Task<IEnumerable<Reel>> GetByWorkspaceAsync(Guid workspaceId, CancellationToken cancellationToken = default);
     Task<IEnumerable<Reel>> SearchByNameAsync(string name, SearchOption searchOption, CancellationToken cancellationToken = default);
 }

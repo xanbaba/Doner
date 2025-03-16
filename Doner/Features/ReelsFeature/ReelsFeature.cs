81795d82b4f9fb770ac9c31bf86dd6cf451e306f
XanbabaF <xanbabaf@gmail.com>
2025-03-16 15:16:57 +0400

    Registering mongo reel collection and reel repo

--- Doner/Features/ReelsFeature/ReelsFeature.cs
+++ Doner/Features/ReelsFeature/ReelsFeature.cs
@@ -1,10 +1,19 @@
+using Doner.Features.ReelsFeature.Services;
+using MongoDB.Driver;
+
 namespace Doner.Features.ReelsFeature;
 
 public class ReelsFeature: IFeature
 {
     public static void Build(WebApplicationBuilder builder)
     {
-        
+        builder.Services.AddSingleton<IMongoCollection<Reel>>(sp =>
+        {
+            var db = sp.GetRequiredService<IMongoDatabase>();
+            return db.GetCollection<Reel>("Reels");
+        });
+
+        builder.Services.AddTransient<IReelRepository, ReelRepository>();
     }
 
     public static void Configure(WebApplication app)

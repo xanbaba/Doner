d52eef3a4e79aab24205d1cec85f12f4391b8bb4
XanbabaF <xanbabaf@gmail.com>
2025-03-18 19:58:07 +0400

    Truncating refresh token to 32 chars to fit "Token" column in RefreshTokens table

--- Doner/Features/AuthFeature/Services/RefreshTokenGenerator.cs
+++ Doner/Features/AuthFeature/Services/RefreshTokenGenerator.cs
@@ -6,7 +6,7 @@ public static class RefreshTokenGenerator
 {
     public static string GenerateRefreshToken()
     {
-        var refreshToken = RandomNumberGenerator.GetBytes(32);
-        return Convert.ToBase64String(refreshToken);
+        var refreshToken = RandomNumberGenerator.GetHexString(32);
+        return refreshToken;
     }
 }

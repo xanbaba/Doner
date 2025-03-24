07bf8a55e13cdb4ecbfde9d898e0de16fcfe9eda
XanbabaF <xanbabaf@gmail.com>
2025-03-24 17:54:16 +0400

    Update Reel model to make Description nullable and add OwnerId property

--- Doner/Features/ReelsFeature/Reel.cs
+++ Doner/Features/ReelsFeature/Reel.cs
@@ -11,9 +11,11 @@ public class Reel
     public Guid Id { get; set; }
     
     public string Name { get; set; } = null!;
-    public string Description { get; set; } = null!;
+    public string? Description { get; set; }
     
     [BsonGuidRepresentation(GuidRepresentation.Standard)]
     public Guid WorkspaceId { get; set; }
+
+    public Guid OwnerId { get; set; }
     public IEnumerable<ReelElement> ReelElements { get; set; } = [];
 }

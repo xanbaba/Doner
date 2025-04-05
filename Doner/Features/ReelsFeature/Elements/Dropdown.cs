a9e7456239da7c26a8a33de0b2523fb27e140824
Khanbaba <28xanbabaf@gmail.com>
2025-04-05 10:50:25 +0400

    Refactor reel elements to inherit from ReelElement and add Id property

--- Doner/Features/ReelsFeature/Elements/Dropdown.cs
+++ Doner/Features/ReelsFeature/Elements/Dropdown.cs
@@ -3,7 +3,7 @@
 namespace Doner.Features.ReelsFeature.Elements;
 
 [BsonDiscriminator(nameof(Dropdown))]
-public class Dropdown
+public class Dropdown : ReelElement
 {
     public IEnumerable<ReelElement> Elements { get; set; } = [];
 }
--- Doner/Features/ReelsFeature/Elements/Picture.cs
+++ Doner/Features/ReelsFeature/Elements/Picture.cs
@@ -3,9 +3,10 @@
 namespace Doner.Features.ReelsFeature.Elements;
 
 [BsonDiscriminator(nameof(Picture))]
-public class Picture
+public class Picture : ReelElement
 {
     public string Url { get; set; } = string.Empty;
     public int Width { get; set; }
     public int Height { get; set; }
+    public string Caption { get; set; } = string.Empty;
 }

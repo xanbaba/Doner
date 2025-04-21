95798d44b25920dba0097b0e448439ad19ec1295
Khanbaba <28xanbabaf@gmail.com>
2025-04-21 23:11:54 +0400

    Remove unused methods from IOTService interface
    Removed the `Transform` and `ApplyOperationAsync` methods from the `IOTService` interface as they were no longer being used. This cleanup simplifies the interface, improving the maintainability and clarity of the codebase.

--- Doner/Features/MarkdownFeature/IOTService.cs
+++ Doner/Features/MarkdownFeature/IOTService.cs
@@ -6,23 +6,6 @@ namespace Doner.Features.MarkdownFeature;
 public interface IOTService
 {
     /// <summary>
-    /// Transform an operation against another operation
-    /// </summary>
-    /// <param name="clientOperation">Client operation to be transformed</param>
-    /// <param name="serverOperation">Server operation to transform against</param>
-    /// <returns>A new operation with transformed components</returns>
-    Operation Transform(Operation clientOperation, Operation serverOperation);
-    
-    /// <summary>
-    /// Apply an operation to a markdown document
-    /// </summary>
-    /// <param name="markdownId">ID of the markdown document</param>
-    /// <param name="operation">Operation to apply</param>
-    /// <param name="cancellationToken">Cancellation token</param>
-    /// <returns>Result of the operation application</returns>
-    Task<ApplyOperationResult> ApplyOperationAsync(string markdownId, Operation operation, CancellationToken cancellationToken = default);
-    
-    /// <summary>
     /// Process an incoming operation (transform if needed, then apply)
     /// </summary>
     /// <param name="operation">Operation to process</param>

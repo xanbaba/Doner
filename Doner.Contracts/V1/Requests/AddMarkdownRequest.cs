10396299e60008d63e93afc70d44f4359be3b4e6
Khanbaba <28xanbabaf@gmail.com>
2025-05-16 15:31:31 +0400

    Add request models for updating and adding markdown
    Introduces `UpdateMarkdownRequest` and `AddMarkdownRequest` classes in the `Contracts.V1.Requests` namespace. These classes define a required `Title` property to facilitate markdown-related API operations.
    
    Signed-off-by: Khanbaba <28xanbabaf@gmail.com>

---
+++ Doner.Contracts/V1/Requests/AddMarkdownRequest.cs
@@ -0,0 +1,6 @@
+namespace Contracts.V1.Requests;
+
+public class AddMarkdownRequest
+{
+    public required string Title { get; set; }
+}
---
+++ Doner.Contracts/V1/Requests/UpdateMarkdownRequest.cs
@@ -0,0 +1,6 @@
+namespace Contracts.V1.Requests;
+
+public class UpdateMarkdownRequest
+{
+    public required string Title { get; set; }
+}
